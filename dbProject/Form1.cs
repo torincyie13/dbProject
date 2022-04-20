7281f02171c46401322774ea6e8fab7cfd761fce
Torin Hawkins <torincyie13@gmail.com>
2022-04-19 21:04:40 -0500

    Add project files.

---
+++ dbProject/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dbProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void membersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.membersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.clutchDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'clutchDBDataSet.Members' table. You can move, or remove it, as needed.
+            this.membersTableAdapter.Fill(this.clutchDBDataSet.Members);
+
+        }
+    }
+}
